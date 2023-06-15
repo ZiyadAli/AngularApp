714d761f857b7a5e4a5a2eb761b2dc97895c341c
Ziyad Daghriri <zey_99@hotmail.com>
2023-06-15 15:03:27 +0300

    up

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
@@ -18,37 +19,53 @@ namespace API.Controllers
         }
 
 
-       [HttpPost("register")] // POST: api/account/register
-       
-       public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        [HttpPost("register")] // POST: api/account/register
+
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
             if (await UserExist(registerDto.Username)) return BadRequest("Username is exist");
             using var hmac = new HMACSHA512();
             var user = new AppUser()
             {
-                UserName = registerDto.Username,
+                UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return Ok(user);
-       }
-       
+            return user;
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto LoginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == LoginDto.Username.ToLower());
+            if (user == null) return Unauthorized();
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(LoginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+                if (computedHash[i] != user.PasswordHash[i])
+                    return Unauthorized("Infalid Password");
+            return user;
+
+        }
+
 
         private async Task<bool> UserExist(string username)
         {
-            return await _context.Users.AnyAsync(x=>x.UserName.ToLower() ==username.ToLower());
+            return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
         }
-       
+
         [HttpGet]
         public ActionResult<IEnumerable<AppUser>> GetUsers()
         {
             return _context.Users.ToList();
         }
-        
+
         [HttpGet("{id}")]
-        public ActionResult<AppUser>GetUsers(int id)
+        public ActionResult<AppUser> GetUsers(int id)
         {
             return _context.Users.Find(id);
         }
